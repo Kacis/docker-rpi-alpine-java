8ce04dc700e19a331fa9b9ae4bce0816ea77ccf2
Milan Kacálek <kacis.m@gmail.com>
2016-12-05 21:23:47 +0000

    Add travis ci config.

--- docker-build.sh
+++ docker-build.sh
@@ -3,4 +3,4 @@
 set -ex
 
 docker run --rm -it -v "$PWD:/usr/local/workdir/" ctarwater/armhf-alpine-rpi-base sh /usr/local/workdir/script/build.sh /usr/local/workdir/
-docker build -t docker-rpi-alpine-java .
+docker build -t kacis/docker-rpi-alpine-java .
