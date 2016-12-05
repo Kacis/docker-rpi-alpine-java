ed86b44ae160fe22b5ad03da44ad0939fc384214
Milan Kacálek <kacis.m@gmail.com>
2016-12-05 21:28:32 +0000

    Change arm alpine for normal alpine.

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-docker run --rm -it -v "$PWD:/usr/local/workdir/" ctarwater/armhf-alpine-rpi-base sh /usr/local/workdir/script/build.sh /usr/local/workdir/
+docker run --rm -it -v "$PWD:/usr/local/workdir/" alpine:3.4 sh /usr/local/workdir/script/build.sh /usr/local/workdir/
 docker build -t kacis/docker-rpi-alpine-java .
